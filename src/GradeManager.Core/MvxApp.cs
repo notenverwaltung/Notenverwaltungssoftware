b99a9879b6fb0dd7c0ec6223775c7acb06f7f52a
tuke307 <tonymeissner70@gmail.com>
2021-01-05 13:46:13 +0100

    add core project with base viewmodels

---
+++ src/GradeManager.Core/MvxApp.cs
@@ -0,0 +1,40 @@
+﻿using MvvmCross;
+using MvvmCross.ViewModels;
+using System.Threading.Tasks;
+
+namespace GradeManager.Core
+{
+    /// <summary>
+    /// BASE Application.
+    /// </summary>
+    /// <seealso cref="MvvmCross.ViewModels.MvxApplication" />
+    public class MvxApp : MvxApplication
+    {
+        /// <summary>
+        /// Any initialization steps that can be done in the background.
+        /// </summary>
+        public override void Initialize()
+        {
+            Mvx.IoCProvider.RegisterSingleton(Plugin.Settings.CrossSettings.Current);
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// If the application is restarted (eg primary activity on Android can be
+        /// restarted) this method will be called before Startup is called again.
+        /// </summary>
+        public override void Reset()
+        {
+            base.Reset();
+        }
+
+        /// <summary>
+        /// Do any UI bound startup actions here.
+        /// </summary>
+        public override Task Startup()
+        {
+            return base.Startup();
+        }
+    }
+}
---
+++ src/GradeManager.Core/ViewModels/ClassManagementViewModel.cs
@@ -0,0 +1,42 @@
+﻿using MvvmCross.Logging;
+using MvvmCross.Navigation;
+using MvvmCross.ViewModels;
+using System.Threading.Tasks;
+
+namespace GradeManager.Core.ViewModels
+{
+    public class ClassManagementViewModel : MvxNavigationViewModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Menu" /> class.
+        /// </summary>
+        /// <param name="logProvider">The log provider.</param>
+        /// <param name="navigationService">The navigation service.</param>
+        /// <param name="messenger">The messenger.</param>
+        public ClassManagementViewModel(IMvxLogProvider logProvider, IMvxNavigationService navigationService)
+            : base(logProvider, navigationService)
+        {
+        }
+
+        #region Methods
+
+        /// <summary>
+        /// Initializes this instance.
+        /// </summary>
+        /// <returns>Initilisierung.</returns>
+        public virtual Task Initialize()
+        {
+            return base.Initialize();
+        }
+
+        /// <summary>
+        /// Prepares this instance. called after construction.
+        /// </summary>
+        public virtual void Prepare()
+        {
+            base.Prepare();
+        }
+
+        #endregion Methods
+    }
+}
